34c607b872e8bf3d717ab9f18423de3050ef0da5
Mark Kadiner <68058426+Asio-otus@users.noreply.github.com>
2020-12-07 23:23:42 +0300

    Add files via upload

---
+++ src/p2-homeworks/h1/HW1.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import AlternativeMessage from "./AlternativeMessage";
+import Message from "./Message";
+
+const messageData = {
+    avatar: "https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg",
+    name: "Mark Kadiner",
+    message: "Not the most elegant solution with the SVG. I will try to figure out the better way for me to put them sprite sheet in state",
+    time: "22:00",
+};
+
+function HW1() {
+    return (
+        <div>
+            homework 1
+            <hr/>
+            <Message
+                avatar={messageData.avatar}
+                name={messageData.name}
+                message={messageData.message}
+                time={messageData.time}
+            />
+
+            {/*для личного творчества, могу проверить*/}
+            <AlternativeMessage/>
+        </div>
+    );
+}
+
+export default HW1;
---
+++ src/p2-homeworks/h1/Message.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import style from "./Message.module.css";
+
+type PropsType = {
+    avatar: string,
+    name: string,
+    message: string,
+    time: string,
+}
+
+function Message(props: PropsType) {
+    return (
+        <div className={style.wrapper}>
+            <img className={style.avatar} src={props.avatar} alt="Avatar"/>
+            <div className={style.messageBubble}>
+                <svg className={style.messageBubble_tail} version="1.1"
+                     baseProfile="full"
+                     xmlns="http://www.w3.org/2000/svg" viewBox="0 0 53.96 79.43">
+                    <path
+                        d="M48.35,79.43H2.89c-2.99,0-4.01-3.97-1.39-5.42c16.98-9.43,46.85-32,46.85-74.01h5.61v79.43H48.35z"/>
+                </svg>
+                <div className={style.name}>
+                    {props.name}
+                </div>
+                <div className={style.message}>
+                    {props.message}
+                </div>
+                <div className={style.time}>
+                    {props.time}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Message;
