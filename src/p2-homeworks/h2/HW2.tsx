596245a4a2b28ed908652d540bbb526080293278
Mark Kadiner <dasistmail765@gmail.com>
2020-12-19 00:06:44 +0300

    HW_2 - Done

--- src/p2-homeworks/h2/HW2.tsx
+++ src/p2-homeworks/h2/HW2.tsx
@@ -22,20 +22,24 @@ const defaultAffairs: Array<AffairType> = [
 // CONSTANTS---
 
 // ---HELPER [PURE-FUNC]
-export const filterAffairs = (affairs: Array<AffairType>, filter: FilterType): any => { // need to fix any
+export const filterAffairs = (affairs: Array<AffairType>, filter: FilterType): Array<AffairType> => {
     if (filter === "all") return affairs;
-    else return; // need to fix
+    else if (filter === "high") return affairs.filter(a => a.priority === "high");
+    else if (filter === "middle") return affairs.filter(a => a.priority === "middle");
+    else if (filter === "low") return affairs.filter(a => a.priority === "low");
+    else return affairs;
 }
-export const deleteAffair = (affairs: Array<AffairType>, _id: number): any => { // need to fix any
-    return; // need to fix
+export const deleteAffair = (affairs: Array<AffairType>, _id: number): Array<AffairType> => {
+    return affairs.filter(a => a._id !== _id);
 }
+// HELPER [PURE-FUNC]---
 
 function HW2() {
-    const [affairs, setAffairs] = useState<Array<AffairType>>(defaultAffairs); // need to fix any
+    const [affairs, setAffairs] = useState<Array<AffairType>>(defaultAffairs);
     const [filter, setFilter] = useState<FilterType>("all");
 
     const filteredAffairs = filterAffairs(affairs, filter);
-    const deleteAffairCallback = (_id: any) => setAffairs(deleteAffair(affairs, _id)); // need to fix any
+    const deleteAffairCallback = (_id: number) => setAffairs(deleteAffair(affairs, _id));
 
     return (
         <div>
@@ -56,6 +60,5 @@ function HW2() {
         </div>
     );
 }
-// HELPER [PURE-FUNC]---
 
 export default HW2;
